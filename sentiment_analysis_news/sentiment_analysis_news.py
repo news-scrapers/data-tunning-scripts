475abdacb756998e4433d73cc500352ac380002b
Hugo J. Bello <hugojose@azlo.com>
2019-05-27 19:05:22 +0200

    started sentiment analysis

---
+++ sentiment_analysis_news/sentiment_analysis_news.py
@@ -0,0 +1,10 @@
+# https://www.pingshiuanchua.com/blog/post/simple-sentiment-analysis-python?utm_campaign=News&utm_medium=Community&utm_source=DataCamp.com
+import nltk
+
+def analyze_new(new):
+nltk.download('vader_lexicon')
+
+
+
+if __name__== "__main__":
+    print("aaa")
