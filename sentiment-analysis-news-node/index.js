c0a8637d07d334adfe145bd0addcc477478973b0
hugojbello <hjbello.wk@gmail.com>
2019-10-04 00:44:03 +0200

    fixed node parts

--- sentiment-analysis-news-node/index.js
+++ sentiment-analysis-news-node/index.js
@@ -1,15 +1,23 @@
 
-const Sentiment = require('sentiment');
-const sentiment = new Sentiment();
+const sentiment = require('multilang-sentiment');
+const fs = require('fs');
 
+const content = 'los gatos son tontos';
+//console.dir(sentiment(content, 'es'));
 
-const analyze = (content) => {
-    const result = sentiment.analyze(content);
-    console.dir(result);
-    return result;
+const path = "../data/processed/processed_news_suicide_data/resultsSuicide.json";
+const pathout = "../data/processed/processed_news_suicide_data/resultsSuicideSentiment.json";
+
+const newsResult = require(path);
+
+for (const new_item of newsResult.news_scraped_result){
+    const sentimentAnalysis = sentiment(new_item.content, 'es')
+    new_item.sentiment_analysis = sentimentAnalysis;
+    console.log(sentimentAnalysis);
 }
 
+fs.writeFileSync(pathout, JSON.stringify(newsResult));
 
-const content = 'cats are stupid';
-const newsResult = require("./resultsSuicide.json");
-analyze(content)
+
+
+
